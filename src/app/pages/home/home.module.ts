abdb1070b8421fcc964a761919840324a94da1be
feliperx <feliipe.xaud@gmail.com>
2023-05-10 20:09:38 -0400

    added small-card component

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuBarComponent } from './menu-bar/menu-bar.component';
 import { BigCardComponent } from './big-card/big-card.component';
+import { SmallCardComponent } from './small-card/small-card.component';
 
 
 
 @NgModule({
   declarations: [
     MenuBarComponent,
-    BigCardComponent
+    BigCardComponent,
+    SmallCardComponent
   ],
   imports: [
     CommonModule
