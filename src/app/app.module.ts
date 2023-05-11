cc04efa616fc5b107167ef34636f6543b795ee7c
feliperx <feliipe.xaud@gmail.com>
2023-05-11 19:42:25 -0400

    adjusted big-card style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import {BigTitleComponent} from './components/big-title/big-title.component';
 import {HomeComponent} from './pages/home/home.component';
 
+import {BigTitleComponent} from './components/big-title/big-title.component';
+import {MenuBarComponent} from './components/menu-bar/menu-bar.component';
+import {BigCardComponent} from './components/big-card/big-card.component';
+import {SmallCardComponent} from './components/small-card/small-card.component';
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    BigTitleComponent
+    BigTitleComponent,
+    MenuBarComponent,
+    BigCardComponent,
+    SmallCardComponent
   ],
   imports: [
     BrowserModule,
