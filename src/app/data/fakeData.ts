67cdf3819d34ac90a133300b6d190c42690009f3
feliperx <feliipe.xaud@gmail.com>
2023-05-13 12:17:15 -0400

    added fakeData

---
+++ src/app/data/fakeData.ts
@@ -0,0 +1,14 @@
+export const fakeData = [
+  {
+    "id":"1",
+    "imageUrl":"",
+    "title":"Titulo",
+    "description":"Descricao"
+  },
+  {
+    "id":"2",
+    "imageUrl":"",
+    "title":"Titulo",
+    "description":"Descricao"
+  }
+]
