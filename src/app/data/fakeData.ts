cdcf19fee102e2e454857ad1790b7bfcf14f6252
feliperx <feliipe.xaud@gmail.com>
2023-05-13 19:34:48 -0400

    added more fakeData

--- src/app/data/fakeData.ts
+++ src/app/data/fakeData.ts
@@ -3,7 +3,7 @@ export const fakeData = [
     id: '1',
     imageUrl:
       'https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/blt209db9312ce8d1fe/6364c712a803a24d15c8884d/Mbappe_Haaland_.jpg',
-    title: 'Haaland ou Mbappe? Quem eh o melhor?',
+    title: 'Haaland ou Mbappe? Quem é o melhor?',
     description: `Tanto Kylian Mbappé quanto Erling Haaland são jogadores jovens excepcionais de futebol com habilidades incríveis, e é difícil dizer definitivamente quem é melhor. Ambos os jogadores estão fazendo ondas no mundo do futebol e são considerados dois dos talentos jovens mais promissores do jogo.
 
     Mbappé, que joga pelo Paris Saint-Germain e pela seleção francesa, é conhecido por sua incrível velocidade, habilidade de drible e habilidades de finalização. Ele ganhou inúmeros prêmios individuais e foi uma parte fundamental do sucesso de sua equipe tanto nacionalmente quanto na Liga dos Campeões.
@@ -14,12 +14,40 @@ export const fakeData = [
   },
   {
     id: '2',
+    imageUrl: 'https://a.espncdn.com/photo/2018/0831/r424075_1296x729_16-9.jpg',
+    title: 'Irmãs Williams',
+    description: `As irmãs Williams, Venus e Serena Williams, são dois dos nomes mais conhecidos e respeitados no mundo do tênis. Elas nasceram e cresceram em Compton, Califórnia, nos Estados Unidos, e começaram a jogar tênis quando ainda eram crianças.
+
+    Venus Williams é a irmã mais velha, nascida em 1980, e começou a jogar tênis profissionalmente em 1994, aos 14 anos. Ela rapidamente se tornou uma das jogadoras mais bem-sucedidas do mundo, ganhando sete títulos de Grand Slam em simples, 14 títulos de Grand Slam em duplas e duas medalhas olímpicas de ouro em duplas.
+
+    Serena Williams, nascida em 1981, é a irmã mais nova e também começou a jogar tênis profissionalmente aos 14 anos. Ela é considerada uma das maiores jogadoras de todos os tempos, tendo ganho 23 títulos de Grand Slam em simples, 14 títulos de Grand Slam em duplas e quatro medalhas olímpicas de ouro em duplas e simples.
+
+    Além de seus impressionantes recordes e realizações no tênis, as irmãs Williams também são conhecidas por sua defesa dos direitos das mulheres e igualdade no esporte. Elas têm sido fortes defensoras da igualdade de pagamento para jogadores de tênis masculinos e femininos e têm trabalhado para promover oportunidades iguais para jovens jogadoras de tênis.
+
+    As irmãs Williams têm sido um modelo para muitos jovens jogadores de tênis, inspirando uma geração de mulheres a perseguir seus sonhos no esporte e fora dele. Sua habilidade, perseverança e determinação ao longo dos anos têm solidificado seu lugar como duas das maiores jogadoras de tênis da história.`
+  },
+  {
+    id: '3',
     imageUrl: 'https://cdn-3.expansion.mx/dims4/default/29611cd/2147483647/strip/true/crop/1670x940+0+0/resize/1200x675!/format/webp/quality/60/?url=https%3A%2F%2Fcherry-brightspot.s3.amazonaws.com%2Ff5%2F2f%2Ff5c57f43426b8c6a057fc4c35192%2Fgettyimages-149135937.jpg',
-    title: 'Michael Phelps eh o melhor nadador de todos os tempos?',
+    title: 'Michael Phelps é o melhor nadador de todos os tempos?',
     description: `O maior nadador do mundo é uma questão de opinião, mas muitas pessoas acreditam que Michael Phelps é um dos maiores nadadores de todos os tempos. Phelps é um atleta olímpico americano que competiu em cinco Olimpíadas diferentes e ganhou um total de 28 medalhas olímpicas, incluindo 23 medalhas de ouro. Ao longo de sua carreira, Phelps estabeleceu inúmeros recordes mundiais e dominou as competições de natação em nível internacional. Ele é conhecido por sua técnica de natação impecável, resistência e habilidade em várias provas, incluindo o nado livre, borboleta e medley individual.
 
     Além de seu sucesso competitivo, Phelps também é conhecido por seu trabalho filantrópico, incluindo seu envolvimento na Fundação Michael Phelps, que promove a natação e ajuda a combater a depressão e o suicídio.
 
     Embora haja muitos nadadores talentosos e bem-sucedidos em todo o mundo, Michael Phelps é amplamente considerado um dos maiores nadadores de todos os tempos, graças a sua incrível carreira e conquistas inigualáveis.`,
   },
+  {
+    id: '4',
+    imageUrl: 'https://www.esportelandia.com.br/wp-content/uploads/2019/12/hortencia-selecao-brasileira-de-basquete.jpg',
+    title: 'Uma das maiores jogadoras de basquete do Brasil e do mundo',
+    description: `Ortência Denise de Sousa Magalhães, mais conhecida como Hortência, é uma das maiores jogadoras de basquete do Brasil e do mundo. Ela nasceu em 23 de setembro de 1959 em Potirendaba, São Paulo, Brasil.
+
+    Hortência começou sua carreira no basquete em Araraquara, onde jogou pelo time local aos 14 anos de idade. Ela rapidamente se destacou como uma jogadora talentosa, e foi convidada para jogar pela seleção brasileira de basquete em 1979, aos 20 anos de idade.
+
+    Durante sua carreira na seleção brasileira, Hortência conquistou inúmeras medalhas e títulos, incluindo a medalha de prata nos Jogos Olímpicos de Los Angeles em 1984 e a medalha de ouro nos Jogos Pan-Americanos de Havana em 1991. Ela também foi eleita a melhor jogadora do mundo pela Federação Internacional de Basquete (FIBA) em 1994.
+
+    Hortência se aposentou do basquete em 1997, após uma carreira de sucesso que a viu marcar mais de 3.000 pontos pela seleção brasileira. Ela também é lembrada por sua personalidade forte e carismática, que a tornou uma figura popular e inspiradora no Brasil.
+
+    Após sua aposentadoria, Hortência trabalhou como comentarista de basquete na televisão brasileira e também atuou como técnica de basquete, treinando a seleção brasileira feminina em 2009. Em 2021, ela foi introduzida no Hall da Fama do Basquete Internacional, em reconhecimento à sua incrível carreira e contribuição para o esporte.`
+  }
 ];
